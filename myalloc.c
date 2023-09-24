94a6ce6e89a47af209d59e4e12ac210d19939d88
Abel Verley <abelmaxv@gmail.com>
2023-09-24 18:52:57 +0200

    fonction myfree

--- myalloc.c
+++ myalloc.c
@@ -3,14 +3,14 @@
 #define MAX_SMALL 100
 
 const int SIZE_BLK_SMALL = 128-sizeof(size_t);
-char small_tab[128*MAX_SMALL];
+static char small_tab[128*MAX_SMALL];
 
 
 void * mymalloc(size_t size){
     if (size<=SIZE_BLK_SMALL){
         for (int b=0; b<MAX_SMALL;b++){
             if(*(size_t*)(small_tab+(128*b))%2==0){
-                return small_tab +(128*b)+sizeof(size_t);
+                return small_tab+(128*b)+sizeof(size_t);
             }
         }
     }
@@ -18,10 +18,16 @@ void * mymalloc(size_t size){
 }
 
 void myfree(void* ptr){
-    if (*((size_t *) ptr -1)%2 == 0){
-        printf("Erreur : le bloc indiqué est libre \n");
+    if (!((size_t) small_tab <= (size_t) ptr <= (size_t) (small_tab + 128*MAX_SMALL)))
+        printf("Error : the pointer given isn't accessible by myfree \n");
+    
+    else if(((size_t) ptr - (size_t) small_tab)%128 != SIZE_BLK_SMALL)
+        printf("Error : the pointer given is not at the begining of a bloc");
+    
+    else if (*((size_t *) ptr -1)%2 == 0){
+        printf("Error : the bloc is empty \n");
     }
     else{
-        
+        *((size_t *) ptr - 1)<<1;
     }
 }
