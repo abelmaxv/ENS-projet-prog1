9477583cbf79ea7ac3d6f4448ccd4a862c06e469
Abel Verley <abelmaxv@gmail.com>
2023-10-02 21:19:52 +0200

    improves visualise_mem + test mymalloc 2

--- myalloc.c
+++ myalloc.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 #define MAX_SMALL 100
+#define SIZE_BLK_SMALL (128-sizeof(size_t))
 
-const size_t SIZE_BLK_SMALL = 128-sizeof(size_t);
 char small_tab[MAX_SMALL*128]; 
 
 
@@ -23,9 +23,11 @@ void myfree(void* ptr){
     if (!((size_t) small_tab <= (size_t) ptr  && (size_t) ptr <= (size_t) (small_tab + 128*MAX_SMALL)))
         printf("Error : the pointer given isn't accessible by myfree \n");
     
-    else if(((size_t) ptr - (size_t) small_tab)%128 != SIZE_BLK_SMALL)
-        printf("Error : the pointer given is not at the begining of a bloc");
-    
+    else if(((size_t) ptr - (size_t) small_tab)%128 != sizeof(size_t)){
+        printf("%lu \n",((size_t) ptr - (size_t) small_tab)%128);
+        printf("%lu \n", SIZE_BLK_SMALL);
+        printf("Error : the pointer given is not at the begining of a bloc \n");
+    }
     else if (*((size_t *) ptr -1)%2 == 0){
         printf("Error : the bloc is empty \n");
     }
@@ -47,18 +49,59 @@ void *myrealloc(void *ptr, size_t size){
     return ptr;
 }
 
-int visualise_mem(int b){
+int visualise_bloc(int b){
     /*Displays content of bloc b*/
     if (b>= MAX_SMALL || b<0){
         printf("Error : bloc out of range \n");
         return 1;
     }
-    printf("Header : %ld \n", *((size_t*)small_tab +b));
+    printf("Displaying bloc %d \n", b);
+    printf("Header : %ld \n", *((size_t*)small_tab +b)); // If header is even then the bloc is free 
     printf("Content : ");
     for(int i = b*128 + sizeof(size_t) ; i<(b+1)*128 ; i++){
         printf("%d", small_tab[i]);
     }
     printf("\n");
+    printf("________________________________________________ \n");
+    return 0;
+}
+
+
+
+int *occ_blocs(){
+    /*Displays the occupied blocs and returns an array of free blocs*/
+    printf("DISPLAYING OCCUPIED BLOCS : \n");
+    int counter = 0;
+    for (size_t b = 0; b< MAX_SMALL; b++){
+        if (*(size_t *)(small_tab + 128*b)%2==1){
+            printf("%lu ; ", b);
+            counter++;
+        }
+    }
+    printf("\n");
+    printf("________________________________________________ \n");
+    // The fist element of ob_tab is counter, then idexes of free blocs
+    int *ob_tab = (int *) malloc((counter+1)*sizeof(int));
+    ob_tab[0]= counter;
+    int tab_index = 1;
+    for (size_t b = 0; b< MAX_SMALL; b++){
+        if (*(size_t *)(small_tab + 128*b)%2==1){
+            ob_tab[tab_index]=b;
+            tab_index++;
+        }
+    }
+    return ob_tab;
+}
+
+
+int visualise_mem(){
+    printf("################################################# \n");
+    int* ob_tab = occ_blocs();
+    for (int i = 1 ; i< ob_tab[0]+1 ;i++){
+        visualise_bloc(ob_tab[i]);
+    }
+    free(ob_tab);
+    printf("################################################# \n");
     return 0;
 }
 
--- myalloc_test.c
+++ myalloc_test.c
@@ -3,15 +3,46 @@
 #include "myalloc.h"
 
 void mymalloc_test1(){
-    /*Tests a bloc is marked as occupied after mymalloc */
+    /*Allocates first bloc of memory then puts a value in it*/
     char *p = (char *) mymalloc(1);
-    visualise_mem(0);
+    visualise_mem();
     *p = 1;
-    visualise_mem(0);
+    visualise_mem();
+
+    /* Test passed */
+}
+
+void myfree_test1(){
+    /*Allocates first bloc of memory then frees it*/
+    char *p = (char *) mymalloc(1);
+    visualise_mem();
+    myfree(p);
+    visualise_mem();
+
+    /* Test passed */
+}
+
+void mymalloc_test2(){
+    /* Allocates 3 blocs of memory, frees the 2de one, then allocates a bloc */
+    char *p = (char *) mymalloc(1);
+    *p =1;
+    char *q = (char *) mymalloc(1);
+    char *r = (char *) mymalloc(1);
+    *r =1;
+    visualise_mem();
+    myfree(q);
+    visualise_mem();
+    char *s = (char *) mymalloc(1);
+    *s =1;
+    visualise_mem();
+
 }
 
 
 int main(){
-    mymalloc_test1();
+    //mymalloc_test1();
+    //myfree_test1();
+    mymalloc_test2();
+
     return 0;
 }
